3a8de5bfee86fb911a1d768c22cf3178b8b3c05e
Jun Han <junhan@microsoft.com>
2017-06-14 15:43:14 +0800

    Pass context object

--- github-webhook/index.js
+++ github-webhook/index.js
@@ -11,7 +11,7 @@ module.exports = function (context, data) {
                 context.log('Service client connected');
                 var message = new Message(`Issue '${data.issue.title}' was ${data.action} at ${data.issue.updated_at}`);
                 context.log('Sending message: ' + message.getData());
-                serviceClient.send(process.env['targetDevice'], message, printResultFor('send'));
+                serviceClient.send(process.env['targetDevice'], message, printResultFor('send', context));
             }
         });
     }
@@ -19,7 +19,7 @@ module.exports = function (context, data) {
     context.done();
 };
 
-function printResultFor(op) {
+function printResultFor(op, context) {
   return function printResult(err, res) {
     if (err) context.log(op + ' error: ' + err.toString());
     if (res) context.log(op + ' status: ' + res.constructor.name);
